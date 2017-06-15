d88b53c2b9cd71cc86f2763a29f473d35d7f02c2
dannygott <dannygott98@gmail.com>
2017-06-14 22:51:14 -0400

    Starting on maps module

--- App.js
+++ App.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Naviago_Map from "./src/props/Naviago_Map.js"
+
+
+
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        <Naviago_Map/>
       </View>
     );
   }
 }
+
+
 
 const styles = StyleSheet.create({
   container: {

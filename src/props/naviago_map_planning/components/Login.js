080bdf00e9fa7eec83645e86de81d92d20604348
Noah Rossi <me@noahrossi.com>
2017-06-19 14:14:52 -0400

    tried it again

--- src/props/naviago_map_planning/components/Login.js
+++ src/props/naviago_map_planning/components/Login.js
@@ -44,6 +44,7 @@ var Login = React.createClass({
                     bottom: 0,
 
               }}
+
               initialRegion={{
                     latitude: 37.78825,
                     longitude: -122.4324,
